6da3edd20c659520cd3330ccd7ebfc54db52d96a
grayoj <Mgeraldoj07@gmail.com>
2023-08-03 12:56:23 +0100

    Refactor: Integrate identity verification client.

--- src/pystack.py
+++ src/pystack.py
@@ -1,10 +1,21 @@
+"""
+Pystack Client - Gerald Maduabuchi (2023)
+MIT LICENSE
+Lightweight client to facilitate authenticating to Paystack's official API.
+Kindly see the documentation for better assistance.
+"""
+
 import http.client
 import json
+from .identity_verification import IdentityVerificationClient
+
 
 class PystackClient:
+
     def __init__(self, pystack_key):
         self.pystack_key = pystack_key
         self.base_url = "https://api.paystack.co"
+        self.identity_verification = IdentityVerificationClient(pystack_key)
     
     def send_request(self, method, path, data=None):
         connection = http.client.HTTPSConnection(self.base_url)
@@ -12,7 +23,7 @@ class PystackClient:
                 "Authorization": f"Bearer {self.pystack_key}",
                 "Content-Type": "application/json",
                 }
-        if data is not None;
+        if data is not None:
             data = json.dumps(data)
 
         connection.request(method, path, data=data, headers=headers)
@@ -28,8 +39,15 @@ class PystackClient:
         }
         response = self._send_request("POST", "/transaction/initialize", data)
         return response
-    
+   
     # Verify payment using reference code.
     def verifyPayment(self, reference):
         response = self._send_request("GET", f"/transaction/verify/{reference}")
         return response
+
+    # Perform identity verification
+    def resolve_card_bin(self, card_bin):
+        return self.identity_verification.resolve_card_bin(card_bin)
+
+    def resolve_account_number(self, account_number, bank_code):
+        return self.identity_verification.resolve_account_number(account_number, bank_code)
